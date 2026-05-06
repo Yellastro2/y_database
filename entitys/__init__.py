fce6f01001e7ed4171b6fcb8481f2f90e15ddec9
Yellastro2 <77885272+Yellastro2@users.noreply.github.com>
2026-05-06 21:36:40 +0300

    Update __init__.py

--- entitys/__init__.py
+++ entitys/__init__.py
@@ -1,2 +1,9 @@
 from y_database.entitys.y_entity import yEntity
 from y_database.entitys.y_conf import yConfig
+
+
+class yConf(yEntity):
+  """Обратная совместимость со старым именем конфигурационной entity."""
+
+  type: str
+  value: str
